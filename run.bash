b429ba4a0c320ecc9a6d3bb867aa8d7fe2e5206b
John Rigler <john@rigler.org>
2020-03-30 13:44:28 -0500

    .

--- run.bash
+++ run.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 YOUR_DOGECOIN_SERVER="root@dime.cash"
-. ./dcovid-19.public.broadcast-46796867584.3467
+. ./dcovid-19.public.broadcast-37534478785.3886
 dcovid-19.public.broadcast > $$
 T=$(php ./sendmany.php $$ | ssh $YOUR_DOGECOIN_SERVER "dd of=cast; . ./cast" )
 echo $T | tee -a ./transactions.txt
