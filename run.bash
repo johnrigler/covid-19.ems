eab56e4344878adb47f4eac92fe2b771554b2afb
John Rigler <john@rigler.org>
2020-03-31 17:40:52 -0500

    Converted run.bash to use a symlink

--- run.bash
+++ run.bash
@@ -2,7 +2,7 @@
 
 YOUR_DOGECOIN_SERVER="root@dime.cash"
 YOUR_LOCAL_DIR="/home/john/covid-19.ems"
-. $YOUR_LOCAL_DIR/dcovid-19.public.broadcast-37534478785.3886
+. $YOUR_LOCAL_DIR/dcovid-19.public.broadcast
 dcovid-19.public.broadcast > $$
 T=$(php $YOUR_LOCAL_DIR/sendmany.php $$ | ssh $YOUR_DOGECOIN_SERVER "dd of=cast; . ./cast" )
 echo $T | tee -a $YOUR_LOCAL_DIR/transactions.txt
